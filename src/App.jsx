34b09a0d52b84e9ea906d23fc1225ea2fb0c861a
Pham Phat <phamphat2003vn@gmail.com>
2024-08-28 10:31:51 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div>Đây là bài tập formik yup tổng hợp về validation dgx</div>
+        </>
+    );
+}
+
+export default App;
